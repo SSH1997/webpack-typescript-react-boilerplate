92abb023f22d2ff53fc5fd5858d816c59f53ac1c
SSH1997 <akasiassh@naver.com>
2021-07-15 12:02:50 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/extensions': ['error', 'never'],
+    'import/no-unresolved': 'off',
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const App: React.FC = () => <div>Wepback-Typescript-React-boilerplate</div>;
+export default App;
